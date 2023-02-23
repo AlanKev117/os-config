7bc2526c70789a896b733d87cbd2927101f77521
Alan Fuentes <alan.fuentes@wizeline.com>
2023-02-23 16:22:00 -0600

    Add files via upload

---
+++ windows_wsl_config.sh
@@ -0,0 +1,6 @@
+export LS_COLORS="${LS_COLORS}ow=0;34:tw=0;34"
+zstyle ':completion:*' list-colors
+
+export WHOME=/mnt/c/Users/fuent
+alias cdw='cd $WHOME'
+alias ls='ls --group-directories-first --color'
