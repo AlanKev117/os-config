31da3487b19ff902e97f72ed8c5427c0a57bcdc0
AlanKev117 <fuentes.alan.98@gmail.com>
2024-10-24 17:39:05 -0600

    Split omz setup in different script

---
+++ linux/setup-omz.sh
@@ -0,0 +1,42 @@
+#!/bin/zsh
+
+set -e
+
+# Install Oh-my-zsh
+if [ -z "${ZSH}" ]
+then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    echo "[INFO] Installed Oh-my-zsh"
+else
+    echo "[INFO] Oh-my-zsh already installed"
+fi
+
+# Install aphrodite theme
+target_theme_dir="~/.oh-my-zsh/custom/themes/aphrodite"
+if [ -d "${target_theme_dir}" ] && [ -d "${target_theme_dir}/.git" ]
+then
+    echo "[INFO] aphrodite repo already cloned in ${target_theme_dir}"
+else
+    git clone --quiet https://github.com/win0err/aphrodite-terminal-theme $target_theme_dir
+fi
+omz theme set aphrodite/aphrodite
+omz reload
+echo "[INFO] aphrodite theme installed"
+
+# Install zsh-syntax-highlighting theme
+target_plugin_dir="${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
+if [ -d "${target_plugin_dir}" ] && [ -d "${target_plugin_dir}/.git" ]
+then
+    echo "[INFO] zsh-syntax-highlighting repo already cloned in ${target_plugin_dir}"
+else
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${target_plugin_dir}    
+fi
+
+if ! grep -q "zsh-syntax-highlighting" ~/.zshrc
+then
+    sed -i '/^plugins=/ s/)/ zsh-syntax-highlighting)/' ~/.zshrc
+fi
+echo "[INFO] Installed omz plugins"
+
+[ "$SHELL" = "/usr/bin/zsh" ] || chsh -s $(which zsh)
+echo "[INFO] set up zsh as default shell"
