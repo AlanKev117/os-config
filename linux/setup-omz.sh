d32e7b12ea6db9d35e4c518c042bb75fac2807ee
AlanKev117 <fuentes.alan.98@gmail.com>
2024-10-24 17:57:11 -0600

    Fixed omz setup script

--- linux/setup-omz.sh
+++ linux/setup-omz.sh
@@ -11,13 +11,16 @@ else
     echo "[INFO] Oh-my-zsh already installed"
 fi
 
+# Source ~/.zshrc source file to load omz
+source ~/.zshrc
+
 # Install aphrodite theme
 target_theme_dir="~/.oh-my-zsh/custom/themes/aphrodite"
 if [ -d "${target_theme_dir}" ] && [ -d "${target_theme_dir}/.git" ]
 then
     echo "[INFO] aphrodite repo already cloned in ${target_theme_dir}"
 else
-    git clone --quiet https://github.com/win0err/aphrodite-terminal-theme $target_theme_dir
+    git clone --quiet https://github.com/win0err/aphrodite-terminal-theme ${target_theme_dir}
 fi
 omz theme set aphrodite/aphrodite
 omz reload
@@ -29,7 +32,7 @@ if [ -d "${target_plugin_dir}" ] && [ -d "${target_plugin_dir}/.git" ]
 then
     echo "[INFO] zsh-syntax-highlighting repo already cloned in ${target_plugin_dir}"
 else
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${target_plugin_dir}    
+    git clone --quiet https://github.com/zsh-users/zsh-syntax-highlighting.git ${target_plugin_dir}
 fi
 
 if ! grep -q "zsh-syntax-highlighting" ~/.zshrc
