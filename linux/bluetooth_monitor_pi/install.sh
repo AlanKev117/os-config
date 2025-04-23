7de76bc80fe4c1470d21452ce7d6a5caf8a1d51a
AlanKev117 <fuentes.alan.98@gmail.com>
2025-04-23 12:45:42 -0600

    Updated comment

--- linux/bluetooth_monitor_pi/install.sh
+++ linux/bluetooth_monitor_pi/install.sh
@@ -10,7 +10,7 @@ GPIO_LED_PIN=${1:-17}
 GPIO_BUTTON_PIN=${2:-27}
 DEVICE_MAC_ADDRESS=${3}
 
-# If no MAC address is provided, the first one of a linked device will be taken
+# If no MAC address is provided, the one of the first linked device is taken
 [ -z "${DEVICE_MAC_ADDRESS}" ] && DEVICE_MAC_ADDRESS=$(bluetoothctl devices | head -1 | awk '{print $2}')
 
 SERVICE_FILE_NAME="bluetooth-monitor.service"
