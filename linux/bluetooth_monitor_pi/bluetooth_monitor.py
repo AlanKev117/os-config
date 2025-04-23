a3d1fceef71d5c2a84f38bb7c98afbbc8e998ce1
AlanKev117 <fuentes.alan.98@gmail.com>
2025-04-23 12:24:15 -0600

    Changed operation mode to focus in connection

--- linux/bluetooth_monitor_pi/bluetooth_monitor.py
+++ linux/bluetooth_monitor_pi/bluetooth_monitor.py
@@ -3,43 +3,51 @@ import time
 import subprocess
 from gpiozero import LED, Button
 
+"""
+Controls the connection to a specific Bluetooth device showing the status
+of the connection in an LED and making possible to toggle it with a button.
+"""
+
 # GPIO pins
 GPIO_BUTTON_PIN = os.environ["GPIO_BUTTON_PIN"]
 GPIO_LED_PIN = os.environ["GPIO_LED_PIN"]
+DEVICE_MAC_ADDRESS = os.environ["DEVICE_MAC_ADDRESS"]
 
 # GPIO objects
 led = LED(GPIO_LED_PIN)
 button = Button(GPIO_BUTTON_PIN, pull_up=False)
 
-def is_bluetooth_on() -> bool:
-    output = subprocess.check_output("bluetoothctl show", shell=True).decode()
-    return "Powered: yes" in output
+def is_connected(device: str) -> bool:
+    output = subprocess.check_output(f"bluetoothctl info {device}", shell=True).decode()
+    return "Connected: yes" in output
 
-def set_bluetooth(state: bool) -> None:
-    action = "power on" if state else "power off"
-    subprocess.run(f"bluetoothctl {action}", shell=True)
+def set_connection(state: bool, device: str) -> None:
+    action = "connect" if state else "disconnect"
+    subprocess.run(f"bluetoothctl {action} {device}", shell=True)
 
-def update_led() -> None:
-    if is_bluetooth_on():
+def update_led(device: str) -> None:
+    if is_connected(device):
         led.on()
     else:
         led.off()
 
-def toggle_bluetooth() -> None:
-    current_state = is_bluetooth_on()
-    set_bluetooth(not current_state)
+def toggle_connection(device: str) -> None:
+    current_state = is_connected(device)
+    set_connection(not current_state, device)
     time.sleep(0.5)
-    update_led()
+    update_led(device)
 
-# Add handler to button when pressed.
-button.when_pressed = toggle_bluetooth
+if __name__ == "__main__":
+    # Add handler to button when pressed.
+    button.when_pressed = lambda: toggle_connection(DEVICE_MAC_ADDRESS)
 
-# Pause process to be run in the background
-print(f"Watching bluetooth status with LED on GPIO pin {GPIO_LED_PIN}. "
-      f"Toggle status by pressing button on GPIO pin {GPIO_BUTTON_PIN}. "
-      "CTRL+C to exit.")
+    # Pause process to be run in the background
+    print(f"Watching status of connection to {DEVICE_MAC_ADDRESS} "
+        f"with LED on GPIO pin {GPIO_LED_PIN}. "
+        f"Toggle status by pressing button on GPIO pin {GPIO_BUTTON_PIN}. "
+        "CTRL+C to exit.")
 
-# Will poll BT status every minute
-while True:
-    update_led()
-    time.sleep(60)
+    # Will poll connection status every few seconds
+    while True:
+        update_led(DEVICE_MAC_ADDRESS)
+        time.sleep(10)
--- linux/bluetooth_monitor_pi/install.sh
+++ linux/bluetooth_monitor_pi/install.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+set -e
+
 # Installs/updates the systemd service that runs the Bluetooth monitor
 # IMPORTANT: run this script inside its folder, `bluetooth_monitor_py/`.
 
-GPIO_LED_PIN = ${1:-17}
-GPIO_BUTTON_PIN = ${2:-27}
+# Optional inputs
+GPIO_LED_PIN=${1:-17}
+GPIO_BUTTON_PIN=${2:-27}
+DEVICE_MAC_ADDRESS=${3}
+
+# If no MAC address is provided, the first one of a linked device will be taken
+[ -z "${DEVICE_MAC_ADDRESS}" ] && DEVICE_MAC_ADDRESS=$(bluetoothctl devices | head -1 | awk '{print $2}')
 
 SERVICE_FILE_NAME="bluetooth-monitor.service"
 
@@ -14,11 +21,12 @@ sudo apt upgrade --yes
 sudo apt install python3-full python3-gpiozero --yes
 
 echo "Creating systemd service..."
-sed "s|{MONITOR_DIR}|$(pwd)|" bluetooth-monitor.service.template > "./${SERVICE_FILE_NAME}"
-sed -i "s/{GPIO_LED_PIN}/${GPIO_LED_PIN}/" "./${SERVICE_FILE_NAME}"
-sed -i "s/{GPIO_BUTTON_PIN}/${GPIO_BUTTON_PIN}/" "./${SERVICE_FILE_NAME}"
+sed "s|{MONITOR_DIR}|$(pwd)|" bluetooth-monitor.service.template > ./${SERVICE_FILE_NAME}
+sed -i "s/{GPIO_LED_PIN}/${GPIO_LED_PIN}/" ./${SERVICE_FILE_NAME}
+sed -i "s/{GPIO_BUTTON_PIN}/${GPIO_BUTTON_PIN}/" ./${SERVICE_FILE_NAME}
+sed -i "s/{DEVICE_MAC_ADDRESS}/${DEVICE_MAC_ADDRESS}/" ./${SERVICE_FILE_NAME}
 
 echo "Updating systemd service..."
-sudo mv "./${SERVICE_FILE_NAME}" "/etc/systemd/system/${SERVICE_FILE_NAME}"
+sudo mv ./${SERVICE_FILE_NAME} /etc/systemd/system/${SERVICE_FILE_NAME}
 sudo systemctl daemon-reload
 sudo systemctl restart ${SERVICE_FILE_NAME}
