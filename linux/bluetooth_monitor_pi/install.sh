3865b1b2029a2c42594460f9820375db6315aa02
AlanKev117 <fuentes.alan.98@gmail.com>
2025-04-24 17:26:08 -0600

    Added support for two connections

--- linux/bluetooth_monitor_pi/install.sh
+++ linux/bluetooth_monitor_pi/install.sh
@@ -5,26 +5,33 @@ set -e
 # Installs/updates the systemd service that runs the Bluetooth monitor
 # IMPORTANT: run this script inside its folder, `bluetooth_monitor_py/`.
 
-# Optional inputs
-GPIO_LED_PIN=${1:-17}
-GPIO_BUTTON_PIN=${2:-27}
-DEVICE_MAC_ADDRESS=${3}
+# Optional command line args:
 
-# If no MAC address is provided, the one of the first linked device is taken
-[ -z "${DEVICE_MAC_ADDRESS}" ] && DEVICE_MAC_ADDRESS=$(bluetoothctl devices | head -1 | awk '{print $2}')
+DEVICE_MAC_ADDRESS=$1 # MAC address of target device to control
+
+GPIO_LED1_PIN=${2:-17} # To show status of connection from interface 1
+GPIO_BUTTON1_PIN=${3:-27} # To control status of connection from interface 1
+
+GPIO_LED2_PIN=${4:-5} # To show status of connection from interface 2
+GPIO_BUTTON2_PIN=${5:-6} # To control status of connection from interface 2
+
+# If no MAC address is provided, we take the one from the first paired device
+[ -z "${DEVICE_MAC_ADDRESS}" ] && DEVICE_MAC_ADDRESS=$(bluetoothctl devices Paired | head -1 | awk '{print $2}')
 
 SERVICE_FILE_NAME="bluetooth-monitor.service"
 
 echo "Installing dependencies..."
 sudo apt update
 sudo apt upgrade --yes
-sudo apt install python3-full python3-gpiozero --yes
+sudo apt install python3-full python3-dev python3-gpiozero  --yes
 
 echo "Creating systemd service..."
-sed "s|{SERVICE_DIR}|$(pwd)|" bluetooth-monitor.service.template > ./${SERVICE_FILE_NAME}
-sed -i "s/{GPIO_LED_PIN}/${GPIO_LED_PIN}/" ./${SERVICE_FILE_NAME}
-sed -i "s/{GPIO_BUTTON_PIN}/${GPIO_BUTTON_PIN}/" ./${SERVICE_FILE_NAME}
+sed "s|{SERVICE_DIR}|$(pwd)|g" bluetooth-monitor.service.template > ./${SERVICE_FILE_NAME}
 sed -i "s/{DEVICE_MAC_ADDRESS}/${DEVICE_MAC_ADDRESS}/" ./${SERVICE_FILE_NAME}
+sed -i "s/{GPIO_LED1_PIN}/${GPIO_LED1_PIN}/" ./${SERVICE_FILE_NAME}
+sed -i "s/{GPIO_BUTTON1_PIN}/${GPIO_BUTTON1_PIN}/" ./${SERVICE_FILE_NAME}
+sed -i "s/{GPIO_LED2_PIN}/${GPIO_LED2_PIN}/" ./${SERVICE_FILE_NAME}
+sed -i "s/{GPIO_BUTTON2_PIN}/${GPIO_BUTTON2_PIN}/" ./${SERVICE_FILE_NAME}
 
 echo "Creating/updating systemd service..."
 sudo mv ./${SERVICE_FILE_NAME} /etc/systemd/system/${SERVICE_FILE_NAME}
