6d7bd22fc3fd4c2092fcec050a0fa2c768151a46
AlanKev117 <fuentes.alan.98@gmail.com>
2025-09-20 16:35:02 -0600

    Added NGINX config and dynamic internal port

--- python/konverter/install.sh
+++ python/konverter/install.sh
@@ -5,6 +5,8 @@ set -e
 SERVICE_DIR=$(pwd)
 ENV_DIR=env
 SERVICE_FILE_NAME=konverter.service
+NGINX_SERVICE_FILE=konverter
+INTERNAL_PORT=5000
 
 if [ -d "${SERVICE_DIR}/${ENV_DIR}" ]
 then
@@ -19,6 +21,7 @@ fi
 echo "Creating systemd service..."
 sed "s|{SERVICE_DIR}|${SERVICE_DIR}|g" ${SERVICE_FILE_NAME}.template > ./${SERVICE_FILE_NAME}
 sed -i "s|{ENV_DIR}|${ENV_DIR}|" ./${SERVICE_FILE_NAME}
+sed -i "s|{PORT}|${INTERNAL_PORT}" ./${SERVICE_FILE_NAME}
 
 echo "Creating/updating systemd service..."
 sudo mv ./${SERVICE_FILE_NAME} /etc/systemd/system/${SERVICE_FILE_NAME}
@@ -33,3 +36,12 @@ else
     sudo systemctl start ${SERVICE_FILE_NAME}
     echo "Service ${SERVICE_FILE_NAME} created!"
 fi
+
+echo "Generating NGINX service..."
+sed "s|{PORT}|${INTERNAL_PORT}|g" ${NGINX_SERVICE_FILE}.template > ./${NGINX_SERVICE_FILE}
+sudo mv ./${SERVICE_FILE_NAME} /etc/nginx/sites-available/${SERVICE_FILE_NAME}
+sudo ln -s /etc/nginx/sites-available/${NGINX_SERVICE_FILE} /etc/nginx/sites-enabled/
+
+echo "Starting/restarting NGINX service..."
+sudo nginx -t
+sudo systemctl restart nginx
